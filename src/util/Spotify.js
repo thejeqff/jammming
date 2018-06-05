e8f56710702f920304e30b2a97a912b56f46aa51
thejeqff <desjeff@gmail.com>
2018-06-05 09:15:54 -0700

    Added third condition to Spotify.js to redirect the user to grant access

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -10,15 +10,16 @@ const redirect = `${urlStart}?${clientId}&${responseType}&${scope}&${redirectUri
 const Spotify = {
   getAccessToken() {
     if (accessToken) {
-      return userToken;
+      return accessToken;
     } else if (window.location.href.match(/access_token=([^&]*)/) &&
   window.location.href.match(/expires_in=([^&]*)/)) {
       accessToken = window.location.href.match(/access_token=([^&]*)/).join('');
       expiresIn = window.location.href.match(/expires_in=([^&]*)/).join('');
       window.setTimeout(() => accessToken = '', expiresIn * 1000);
       window.history.pushState('Access Token', null, '/');
+      return accessToken;
     } else {
-
+      window.location = redirect;
     }
   };
 }
