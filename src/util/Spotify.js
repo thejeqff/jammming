93db66e894e2fb6185cda48dc8a447e4c72953a6
thejeqff <desjeff@gmail.com>
2018-06-05 09:01:08 -0700

    reworked to Spotify.js to correctly check for accessToken and check URL for accessToken

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,17 +1,24 @@
-let userToken = '';
+let accessToken = '';
+let expiresIn = '';
 const urlStart = 'https://accounts.spotify.com/authorize?'
 const clientId = 'client_id=02829885ac4a43d687963cfc747cc72f&';
 const responseType = 'response_type=token&';
+const scope = 'scope=playlist-modify-public&'
 const redirectUri = 'redirect_uri=http://localhost:3000/';
-const redirect = urlStart + clientId + responseType + redirectUri;
+const redirect = `${urlStart}${clientId}${responseType}${scope}${redirectUri}`;
 
 const Spotify = {
   getAccessToken() {
-    if (userToken) {
+    if (accessToken) {
       return userToken;
+    } else if (window.location.href.match(/access_token=([^&]*)/) &&
+  window.location.href.match(/expires_in=([^&]*)/)) {
+      accessToken = window.location.href.match(/access_token=([^&]*)/).join('');
+      expiresIn = window.location.href.match(/expires_in=([^&]*)/).join('');
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
     } else {
-      window.location = url;
-      window.location.href.match()
+
     }
   };
 }
